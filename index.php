b0a252b9d5518121904458962ec1fc4376ad903b
tidave85 <davidgagne85@gmail.com>
2016-03-08 09:21:50 -0500

    Remove unwanted comment lines

--- index.php
+++ index.php
@@ -22,7 +22,6 @@
 	<!--[if lt IE 8]>
 	<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 	<![endif]-->
-	<!-- Added commentary -->
 	<div class="container-fluid">
 		<div id="home" class="row">
 			<div class="col-md-12">
